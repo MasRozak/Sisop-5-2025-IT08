e27ac240b41eb4c836a1e1f821c867621c34b67f
aryarefman <155049856+aryarefman@users.noreply.github.com>
2025-05-31 12:17:45 +0700

    Update kernel.c

--- src/kernel.c
+++ src/kernel.c
@@ -6,17 +6,43 @@ int main() {
   shell();
 }
 
-void printString(char *str)
-{
-  //TODO: Implementasi fungsi untuk mencetak string
+void printString(char *str) {
+  int i = 0;
+  while (str[i] != '\0') {
+    interrupt(0x10, 0x0E00 | str[i], 0, 0, 0);
+    i++;
+  }
 }
 
-void readString(char *buf)
-{
-  //TODO: Implementasi fungsi untuk membaca string
+void readString(char *buf) {
+  int index = 0;
+  char c;
+  do {
+    c = (char)interrupt(0x16, 0x0000, 0, 0, 0);
+    
+    if (c == 0x0D) {
+      buf[index] = '\0';
+      interrupt(0x10, 0x0E00 | '\r', 0, 0, 0); 
+      interrupt(0x10, 0x0E00 | '\n', 0, 0, 0); 
+      break;
+    } else if (c == 0x08 && index > 0) {
+      index--;
+      interrupt(0x10, 0x0E00 | 0x08, 0, 0, 0);
+      interrupt(0x10, 0x0E00 | ' ', 0, 0, 0);
+      interrupt(0x10, 0x0E00 | 0x08, 0, 0, 0);
+    } else if (c >= 0x20 && index < 127) {
+      buf[index] = c;
+      index++;
+      interrupt(0x10, 0x0E00 | c, 0, 0, 0);
+    }
+  } while (true);
 }
 
-void clearScreen()
-{
-  //TODO: Implementasi fungsi untuk membersihkan layar
+void clearScreen() {
+  int i;
+  for (i = 0; i < 80 * 25 * 2; i += 2) {
+    putInMemory(0xB800, i, ' ');      
+    putInMemory(0xB800, i + 1, 0x07); 
+  }
+  interrupt(0x10, 0x0200, 0, 0, 0);
 }
